ad29e5e3dea9c3b4ca7aeb7702948a76693da345
Wiotrek <piotrwaszak97@gmail.com>
2020-07-04 09:52:24 +0200

    already index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="HTML, CSS, JavaScript">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projekt3</title>
+    <script src="https://kit.fontawesome.com/03d927952c.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="theme.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.js" integrity="sha256-DrT5NfxfbHvMHux31Lkhxg42LY6of8TaYyK50jnxRnM=" crossorigin="anonymous"></script>
+    <script src="headScript.js"></script>
+</head>
+
+<body>
+    <header>
+        <h1>Sklep</h1>
+    </header>
+    <menu>
+        <ul>
+            <li>Zaloguj się <i class="fas fa-sign-in-alt"></i></li>
+            <li>Zarejestruj się</li>
+        </ul>
+    </menu>
+    <main>
+        <div class='mainHeadQuad'>
+            <h1>Witaj w sklepie</h1>
+            <h2>Mamy nadzieje ze spodobaja Ci sie nasze produkty</h2>
+            <a href='#'><h2 class='mainLookProducts'>Zobacz nasze produkty</h2></a>
+        </div>
+    </main>
+</body>
+</html>
